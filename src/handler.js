bb462bf0786692be6828619edd931e123ecd9eb9
Santiago Aday <santiagoaday7@gmail.com>
2024-07-06 19:41:18 -0400

    Added slack notification

--- src/handler.js
+++ src/handler.js
@@ -1,8 +1,22 @@
-const axios = require("axios");
-const qs = require("qs");
+const { WebClient } = require("@slack/web-api");
+
+const slackToken = process.env.SLACK_ACCESS_TOKEN;
+const slackChannel = "#linear-updates";
+
+const slackClient = new WebClient(slackToken);
 
 exports.handler = async (event) => {
   console.log(event);
+
+  try {
+    await slackClient.chat.postMessage({
+      channel: slackChannel,
+      text: `New event received: ${JSON.stringify(event)}`,
+    });
+    console.log("Message sent to Slack");
+  } catch (error) {
+    console.error("Error sending message to Slack:", error);
+  }
 
   return {
     statusCode: 200,
