34e02c0e8b539f020f04757ae2a7f1bb154db58e
Santiago Aday <santiagoaday7@gmail.com>
2024-07-06 18:51:19 -0400

    fixed env variables

--- src/handler.js
+++ src/handler.js
@@ -2,43 +2,7 @@ const axios = require("axios");
 const qs = require("qs");
 
 exports.handler = async (event) => {
-  const invoiceId = event.pathParameters.invoiceId;
-  const clientId = process.env.PAYPAL_CLIENT_ID;
-  const clientSecret = process.env.PAYPAL_CLIENT_SECRET;
-
-  const tokenResponse = await axios.post(
-    "https://api-m.paypal.com/v1/oauth2/token",
-    qs.stringify({ grant_type: "client_credentials" }),
-    {
-      auth: {
-        username: clientId,
-        password: clientSecret,
-      },
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    }
-  );
-
-  const paypalAccessToken = tokenResponse.data.access_token;
-
   console.log(event);
-  console.log(invoiceId);
-  console.log(event.pathParameters);
-
-  // Fetch Invoice Data
-  const response = await axios.get(
-    `https://api-m.paypal.com/v2/invoicing/invoices/${invoiceId}`,
-    {
-      headers: {
-        Authorization: `Bearer ${paypalAccessToken}`,
-      },
-    }
-  );
-
-  console.log(response);
-
-  const invoice = response.data;
 
   return {
     statusCode: 200,
